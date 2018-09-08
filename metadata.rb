7bcced2657abcf39c3a8c02b789e6a48f90268e8
Adam Linkous <alinkous@gmail.com>
2018-09-08 14:08:06 -0500

    Update Wrapper To Use Latest Version Of Cookbook
    - Bump versions in metadata
    - Improve rakefile

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'alinkous+support@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mywrapper_chef-client'
 long_description 'Installs/Configures mywrapper_chef-client'
-version '0.1.1'
+version '0.2.0'
 supports 'centos'
 chef_version '>= 12.19' if respond_to?(:chef_version)
 issues_url 'https://github.com/gryte/mywrapper_chef-client/issues'
 source_url 'https://github.com/gryte/mywrapper_chef-client'
 
-depends 'chef-client', '~> 10.0.4'
+depends 'chef-client', '~> 11.0.0'
